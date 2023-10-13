be5d4c9a2270071482ec8bc062d003bcb3ce7d08
ISAWASHUN <syun20000616@icloud.com>
2023-10-13 23:41:31 +0900

    first commit

---
+++ ui-test-lesson/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AsyncComponent from './components/AsyncComponent'
+
+function App() {
+
+  return (
+    <>
+    <AsyncComponent />
+    </>
+  )
+}
+
+export default App
---
+++ ui-test-lesson/src/components/Form.stories.tsx
@@ -0,0 +1,25 @@
+import { within } from "@testing-library/react";
+import Form from "./Form";
+import type { Meta, StoryObj } from "@storybook/react";
+import userEvent from "@testing-library/user-event";
+
+const meta = {
+  title: "Form",
+  component: Form,
+} as Meta<typeof Form>
+
+export default meta
+
+type Story = StoryObj<typeof Form>
+
+export const Default: Story = {}
+
+export const Testing: Story = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+    const input = canvas.getByRole("textbox")
+    await expect(input).toHaveTextContent("")
+    await userEvent.type(input, "play function")
+    await expect(canvas.getByDisplayValue("play function")).toBeInTheDocument()
+  }
+}
